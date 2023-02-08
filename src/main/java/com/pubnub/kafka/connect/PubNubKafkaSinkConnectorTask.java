ef37924c4a5497d58a1a7dc1de7eabc0339ab14c
Stephen Blum <blum.stephen@gmail.com>
2023-02-08 13:19:03 -0800

    PubNub SDK added and publishing compile success.

--- src/main/java/com/pubnub/kafka/connect/PubNubKafkaSinkConnectorTask.java
+++ src/main/java/com/pubnub/kafka/connect/PubNubKafkaSinkConnectorTask.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
+
+import com.pubnub.api.UserId;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNubException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,9 +25,9 @@ public class PubNubKafkaSinkConnectorTask extends SinkTask {
     private static final String NUMERIC_COLUMN = "numeric-column";
     private static final String BOOLEAN_COLUMN = "boolean-column";
 
-    private final Random random = new Random(System.currentTimeMillis());
     private final Logger log = LoggerFactory.getLogger(PubNubKafkaSinkConnectorTask.class);
 
+    private PubNub pubnub;
     private PubNubKafkaConnectorConfig config;
     private int taskSleepTimeout;
     private List<String> sources;
@@ -36,16 +40,49 @@ public class PubNubKafkaSinkConnectorTask extends SinkTask {
     @Override
     public void start(Map<String, String> properties) {
         config = new PubNubKafkaConnectorConfig(properties);
-        taskSleepTimeout = config.getInt(TASK_SLEEP_TIMEOUT_CONFIG);
-        String sourcesStr = properties.get("sources");
-        sources = Arrays.asList(sourcesStr.split(","));
+        try {
+            final UserId userId = new UserId("myUniqueUserId");
+            PNConfiguration pnConfiguration = new PNConfiguration(userId);
+            pnConfiguration.setSubscribeKey("demo");
+            pnConfiguration.setPublishKey("demo");
+            //pnConfiguration.setSecretKey("demo");
+            pubnub = new PubNub(pnConfiguration);
+            //TODO remove and replace with other.
+            taskSleepTimeout = config.getInt(TASK_SLEEP_TIMEOUT_CONFIG);
+            String sourcesStr = properties.get("sources"); // "channels"
+            sources = Arrays.asList(sourcesStr.split(","));
+            //TODO remove and replace with other.
+        }
+        catch(PubNubException error) {
+            log.error("Unable to initialize PubNub Connection", error);
+        }
+    }
+
+    private void publish(SinkRecord record) {
+        pubnub.publish()
+            .channel(record.topic())
+            .message(record.value())
+            .async((result, publishStatus) -> {
+                if (publishStatus.isError()) {
+                    log.error(" ⛔️ PUBLISHING TO PUBNUB FAILED");
+                    // Message successfully published to specified channel.
+                }
+                // Request processing failed.
+                else {
+                    log.info(" ✅ PUBLISHING TO PUBNUB Success!");
+                    // Handle message publish error
+                    // Check 'category' property to find out
+                    // issues because of which the request failed.
+                    // Request can be resent using: [status retry];
+                }
+            });
     }
 
     @Override
     public void put(Collection<SinkRecord> records) {
         for (final SinkRecord record : records) {
-            //pubnub.publishFrom(record);
-            log.error("PUBLISHING TO PUBNUB");
+            log.info(" ->->-> PUBLISHING TO PUBNUB <-<-<- ");
+            publish(record);
         }
     }
 
@@ -56,7 +93,7 @@ public class PubNubKafkaSinkConnectorTask extends SinkTask {
 
     @Override
     public void stop() {
-        log.error("Stopping PubNub Sink Connector Task");
+        log.info("Stopping PubNub Sink Connector Task");
     }
 
 }
